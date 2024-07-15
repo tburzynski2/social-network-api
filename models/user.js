aaf1a047c072a38000c94618e29be5f7e043d8c4
Tom Bee <tburzynski2@yahoo.com>
2024-07-15 16:47:36 -0500

    User model schema

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+// Schema for User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must be a valid email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property that gets the amount of friends per user
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
